3bcf299b93044fbdad0e860791b105ba57729f76
Unknown <YGodinez594@west-mec.org>
2018-02-22 14:01:52 -0700

    Added the use of strict.
    Debugged the website and added the use of strict.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# S1_Project_07_04.4
+# S1_Project_07_04.3
 JavaScript: Debugging and Error Handling
 
 
-Specifications for this project can be found here: https://docs.google.com/document/d/1u_y5syMVqp0_G_vz6pArlXGgtJ-SnqK5Q2ynTT_NuZs/edit?usp=sharing
+Specifications for this project can be found here: https://docs.google.com/document/d/1xXjRCWdkZa40B4Z9bTaYHNU8NJ8MpTCYHNuPJ5U_kbk/edit?usp=sharing
---
+++ work.js
@@ -0,0 +1,29 @@
+var places = []; // new array to store entered places
+var i = 1; // counter variable to track array indexes
+
+// function to add input to array and then generate list after 5th submission
+"use strict";
+function processInput() {
+   places[i] = document.getElementById("placeBox").value; // add entered value to array
+   document.getElementById("placeBox").value = "" // clear text box
+   if (i < 5) { // iterate counter variable
+      i++;
+   }
+   else { // add entered value to array and write results to document
+      document.getElementById("resultsExpl").innerHTML = "You entered the following places:";
+    var listItem = "";
+      for (j = 1; j < 6; j++) { // write each array element to its corresponding list item
+         listItem = "item" + j;
+         document.getElementById(listItem).innerHTML = places[j];
+      }
+
+}
+}
+
+// add backward compatible event listener to Submit button
+var submitButton = document.getElementById("button");
+if (submitButton.addEventListener) {
+  submitButton.addEventListener("click", processInput, false);
+} else if (submitButton.attachEvent)  {
+  submitButton.attachEvent("onclick", processInput);
+}
